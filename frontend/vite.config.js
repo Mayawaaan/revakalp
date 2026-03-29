b85d8e88345877d2e9959ac12ce5a908da317b81
Ayan Magardey <ayanmagardey@gmail.com>
2026-03-30 02:10:19 +0530

    Changes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://revakalp-2461.onrender.com',
         changeOrigin: true,
       },
     },
