211789a409daf8792e23dba76d4f1b86dfb2d029
Ayan Magardey <ayanmagardey@gmail.com>
2026-06-07 02:29:07 +0530

    ui changes

--- frontend/src/pages/shop/ProductListPage.jsx
+++ frontend/src/pages/shop/ProductListPage.jsx
@@ -60,7 +60,7 @@ const ProductListPage = () => {
 
         {/* Products */}
         {filteredProducts.length > 0 ? (
-          <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 md:gap-6">
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 md:gap-6">
             {filteredProducts.map((product) => (
               <div
                 key={product._id || product.id}
